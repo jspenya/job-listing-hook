f683e3efe0e93c70991c000b4b3c13623ccaf60f
Tepen <elcabron.penyah@gmail.com>
2021-06-14 18:47:19 +0800

    adding New Jobs component // create jobs & persist to database

---
+++ src/components/NewJobForm.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const NewJobForm = props => {
+  const [job, setJob] = useState(props.initialFormState);
+
+  const handleInputChange = event => {
+    const { name, value } = event.target
+    setJob({...job, [name]: value })
+  };
+
+  return(
+    <form onSubmit={event => {
+      event.preventDefault()
+
+      if (!job.company || !job.position || !job.description) return;
+      props.addJob(job)
+      setJob(props.initialFormState)
+    }}>
+    <label>Company</label>
+    <input type="text" name="company" value={job.company} onChange={handleInputChange}></input>
+    <label>Position</label>
+    <input type="text" name="position" value={job.position} onChange={handleInputChange}></input>
+    <label>Description</label>
+    <input type="text" name="description" value={job.description} onChange={handleInputChange}></input>
+    <button>Create Job</button>
+    </form>
+  )
+};
+
+export default NewJobForm;
